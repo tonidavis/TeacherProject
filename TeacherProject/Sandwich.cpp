58e42a3db5a9935d6335c868cfc69ad84ca62bea
tonidavis <cody.henrichsen@740s-pl-114.local>
2016-07-26 12:25:37 -0600

    Created Sandwich.cpp file

---
+++ TeacherProject/Sandwich.cpp
@@ -0,0 +1,9 @@
+//
+//  Sandwich.cpp
+//  TeacherProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "Sandwich.h"
---
+++ TeacherProject/Sandwich.h
@@ -0,0 +1,14 @@
+//
+//  Sandwich.hpp
+//  TeacherProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef Sandwich_hpp
+#define Sandwich_hpp
+
+#include <stdio.h>
+
+#endif /* Sandwich_hpp */
