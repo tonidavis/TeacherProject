f8dcdfff752b3bd391359dc5fe2fc1695f062c8b
tonidavis <cody.henrichsen@740s-pl-114.local>
2016-07-26 11:29:41 -0600

    Added sample lines to code.

--- TeacherProject/main.cpp
+++ TeacherProject/main.cpp
@@ -8,8 +8,23 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+using namespace std;
+
+int main()
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    double myFraction;
+    cout << myFraction << endl;
+    myFraction = 3.15;
+    cout << myFraction << endl;
+    int myFavoriteNumber;
+    cin >> myFavoriteNumber;
+    cout << myFavoriteNumber <<endl;
+    bool myBoolean = true;
+    cout << myBoolean <<endl;
+    string myWords;
+    myWords = "lsdfjaksdjf";
+    cout << myWords << endl;
     return 0;
 }
